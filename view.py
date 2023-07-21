3366c6e912aef29ee272fb2e171333ff3010f918
Ashish madhukar morokar <ashishmorokar@gmail.com>
2023-07-21 10:29:04 +0530

    view.py

---
+++ view.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Movie
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/movie_list.html', {'movies': movies})
